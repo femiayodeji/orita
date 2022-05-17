740b2853805bddaaa1bade913229b59eca4965c7
Femi Ayodeji <femidotayodeji@gmail.com>
2022-05-17 13:12:58 +0100

    Share video stream

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const { env } = require('process');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, { debug: true });
 
 const port = process.env.PORT || 3000;
 server.listen(port);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -18,4 +22,10 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room });
 });
 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+    })
+})
 console.log("connected and listening at http://localhost:%s", port);
--- public/script.js
+++ public/script.js
@@ -1,15 +1,62 @@
+const socket = io('/');
+
 const videoGrid = document.getElementById('video-grid');
 const videoDOM = document.createElement('video');
 videoDOM.muted = true;
 
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3000'
+});
+const ROOM_ID = "<%= roomId %>";
 let videoStream;
+
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true
+    // audio: true
 }).then(stream => {
     videoStream = stream;
-    addVideoStream(videoDOM, stream)
+    addVideoStream(videoDOM, stream);
+
+    // peer.on('call', call => {
+    //     console.log("answering...");
+    //     call.answer(stream);
+    //     const video = document.createElement('video')
+    //     call.on('stream', userVideoStream => {
+    //         addVideoStream(video, userVideoStream)
+    //     });
+    // });
+
+    socket.on('user-connected', (userId) => {
+        connectNewUser(userId, stream);
+    });
+}).catch((e) => {
+    console.log("Error", e);
 })
+
+peer.on('open', id => {
+    console.log("opening...");
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+peer.on('call', call => {
+    console.log("answering...");
+    call.answer(videoStream);
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    });
+});
+
+const connectNewUser = (userId, stream) => {
+    console.log("calling...");
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    });
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
