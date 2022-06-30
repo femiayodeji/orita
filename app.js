12651ff9fd9d4087bdf15196fea1ae47ce21bd98
iobami <ayobamialadenoye@yahoo.com>
2022-06-30 18:38:00 +0100

    cross origin

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const { env } = require('process');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3002', // client app domain
+        methods: ['GET', 'POST']
+    }
+});
 const { v4: uuidv4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, { debug: true });
@@ -25,7 +30,8 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
+        // socket.to(roomId).emit('user-connected', userId); // not emitting on client side
+        socket.emit('user-connected', userId);
         // socket.on('ready', () => {
         //     socket.to(roomId).emit('user-connected', userId);
         // })
@@ -36,4 +42,7 @@ io.on('connection', socket => {
 
     })
 })
+
+io.listen(3001); // socket to listen on diff port to avoid invalid frame header error in cross origin
+
 console.log("connected and listening at http://localhost:%s", port);
